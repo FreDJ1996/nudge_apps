d5136ae65e17af72f7276bbfecf5fb67afad042c
FrederikJobst <frederik@klesberg.de>
2024-06-11 06:41:40 +0000

    First Running Version of powernudge

--- custom_components/powernudge/config_flow.py
+++ custom_components/powernudge/config_flow.py
@@ -1,81 +1,85 @@
-"""Adds config flow for Blueprint."""
+"""Config flow for test integration."""
 
-from __future__ import annotations
 
 import voluptuous as vol
-from homeassistant import config_entries, data_entry_flow
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant import config_entries
+from homeassistant.components.sensor.const import DOMAIN as SENSOR_DOMAIN
+from homeassistant.const import CONF_NAME
 from homeassistant.helpers import selector
-from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
-from .api import (
-    IntegrationBlueprintApiClient,
-    IntegrationBlueprintApiClientAuthenticationError,
-    IntegrationBlueprintApiClientCommunicationError,
-    IntegrationBlueprintApiClientError,
+from .const import (
+    CONF_GOAL_TYPE,
+    CONF_METER_DELTA_VALUES,
+    CONF_METER_NET_CONSUMPTION,
+    CONF_METER_OFFSET,
+    CONF_METER_PERIODICALLY_RESETTING,
+    CONF_NUDGE_GOAL,
+    CONF_SENSOR_ALWAYS_AVAILABLE,
+    CONF_SOURCE_PERSON,
+    CONF_SOURCE_SENSOR,
+    DOMAIN,
 )
-from .const import DOMAIN, LOGGER
+
+GOAL_TYPES = ["TAG", "MONAT", "JAHR"]
 
 
-class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for Blueprint."""
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAME): selector.TextSelector(),
+        vol.Required(CONF_SOURCE_SENSOR): selector.EntitySelector(
+            selector.EntitySelectorConfig(domain=SENSOR_DOMAIN),
+        ),
+        vol.Required(CONF_METER_OFFSET): selector.NumberSelector(
+            selector.NumberSelectorConfig(
+                min=0,
+                max=28,
+                mode=selector.NumberSelectorMode.BOX,
+                unit_of_measurement="days",
+            ),
+        ),
+        vol.Required(
+            CONF_METER_NET_CONSUMPTION
+        ): selector.BooleanSelector(),
+        vol.Required(
+            CONF_METER_DELTA_VALUES
+        ): selector.BooleanSelector(),
+        vol.Required(
+            CONF_METER_PERIODICALLY_RESETTING
+        ): selector.BooleanSelector(),
+        vol.Optional(
+            CONF_SENSOR_ALWAYS_AVAILABLE
+        ): selector.BooleanSelector(),
+        vol.Required(CONF_GOAL_TYPE): selector.SelectSelector(
+            selector.SelectSelectorConfig(options=GOAL_TYPES)
+        ),
+        vol.Required(CONF_NUDGE_GOAL): selector.NumberSelector(
+            selector.NumberSelectorConfig(
+                min=0,
+                max=1000,
+                mode=selector.NumberSelectorMode.SLIDER,
+                unit_of_measurement="kWh",
+            )
+        ),
+    }
+)
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Example config flow."""
 
     VERSION = 1
+    # The schema version of the entries that it creates
+    # Home Assistant will call your migrate method if the version changes
 
-    async def async_step_user(
-        self,
-        user_input: dict | None = None,
-    ) -> data_entry_flow.FlowResult:
-        """Handle a flow initialized by the user."""
-        _errors = {}
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
         if user_input is not None:
-            try:
-                await self._test_credentials(
-                    username=user_input[CONF_USERNAME],
-                    password=user_input[CONF_PASSWORD],
-                )
-            except IntegrationBlueprintApiClientAuthenticationError as exception:
-                LOGGER.warning(exception)
-                _errors["base"] = "auth"
-            except IntegrationBlueprintApiClientCommunicationError as exception:
-                LOGGER.error(exception)
-                _errors["base"] = "connection"
-            except IntegrationBlueprintApiClientError as exception:
-                LOGGER.exception(exception)
-                _errors["base"] = "unknown"
-            else:
-                return self.async_create_entry(
-                    title=user_input[CONF_USERNAME],
-                    data=user_input,
-                )
+            return self.async_create_entry(
+                title=user_input[CONF_NAME], data=user_input
+            )
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(
-                        CONF_USERNAME,
-                        default=(user_input or {}).get(CONF_USERNAME, vol.UNDEFINED),
-                    ): selector.TextSelector(
-                        selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.TEXT,
-                        ),
-                    ),
-                    vol.Required(CONF_PASSWORD): selector.TextSelector(
-                        selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.PASSWORD,
-                        ),
-                    ),
-                },
-            ),
-            errors=_errors,
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
-
-    async def _test_credentials(self, username: str, password: str) -> None:
-        """Validate credentials."""
-        client = IntegrationBlueprintApiClient(
-            username=username,
-            password=password,
-            session=async_create_clientsession(self.hass),
-        )
-        await client.async_get_data()
--- custom_components/powernudge/sensor.py
+++ custom_components/powernudge/sensor.py
@@ -1,57 +1,203 @@
-"""Sensor platform for powernudge."""
+"""Platform for sensor integration."""
 
 from __future__ import annotations
 
+import logging
+from datetime import timedelta
 from typing import TYPE_CHECKING
 
-from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
+from homeassistant.components.sensor.const import SensorDeviceClass
+from homeassistant.components.utility_meter.const import (
+    BIMONTHLY,
+    DAILY,
+    HOURLY,
+    MONTHLY,
+    QUARTER_HOURLY,
+    QUARTERLY,
+    WEEKLY,
+    YEARLY,
+)
+from homeassistant.components.utility_meter.sensor import UtilityMeterSensor
+from homeassistant.core import (
+    HomeAssistant,
+    callback,
+)
+from homeassistant.helpers import (
+    entity_registry as er,
+)
+from homeassistant.helpers import label_registry as lr
+from homeassistant.helpers.device_registry import DeviceInfo
 
-from .entity import IntegrationBlueprintEntity
-
-if TYPE_CHECKING:
-    from homeassistant.core import HomeAssistant
-    from homeassistant.helpers.entity_platform import AddEntitiesCallback
-
-    from .coordinator import BlueprintDataUpdateCoordinator
-    from .data import IntegrationBlueprintConfigEntry
-
-ENTITY_DESCRIPTIONS = (
-    SensorEntityDescription(
-        key="powernudge",
-        name="Integration Sensor",
-        icon="mdi:format-quote-close",
-    ),
+from .const import (
+    CONF_GOAL_TYPE,
+    CONF_METER_DELTA_VALUES,
+    CONF_METER_NET_CONSUMPTION,
+    CONF_METER_OFFSET,
+    CONF_METER_PERIODICALLY_RESETTING,
+    CONF_METER_TYPE,
+    CONF_NUDGE_GOAL,
+    CONF_SENSOR_ALWAYS_AVAILABLE,
+    CONF_SOURCE_SENSOR,
 )
 
+if TYPE_CHECKING:
+    from homeassistant.config_entries import ConfigEntry
+    from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
+METER_TYPES = [
+    "none",
+    QUARTER_HOURLY,
+    HOURLY,
+    DAILY,
+    WEEKLY,
+    MONTHLY,
+    BIMONTHLY,
+    QUARTERLY,
+    YEARLY,
+]
+
+_LOGGER = logging.getLogger(__name__)
+
+ATTR_SOURCE_ID = "source"
+ATTR_STATUS = "status"
+ATTR_PERIOD = "meter_period"
+ATTR_LAST_PERIOD = "last_period"
+ATTR_LAST_VALID_STATE = "last_valid_state"
+ATTR_TARIFF = "tariff"
+
+PRECISION = 3
+PAUSED = "paused"
+COLLECTING = "collecting"
+
+
+
+
+# Note how both entities for each roller sensor (battry and illuminance) are added at
+# the same time to the same list. This way only a single async_add_devices call is
+# required.
 async def async_setup_entry(
-    hass: HomeAssistant,  # noqa: ARG001 Unused function argument: `hass`
-    entry: IntegrationBlueprintConfigEntry,
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
-    """Set up the sensor platform."""
-    async_add_entities(
-        IntegrationBlueprintSensor(
-            coordinator=entry.runtime_data.coordinator,
-            entity_description=entity_description,
-        )
-        for entity_description in ENTITY_DESCRIPTIONS
+    """Initialize Powernudge config entry."""
+    entry_id = config_entry.entry_id
+    registry = er.async_get(hass)
+    # Validate + resolve entity registry id to entity_id
+    source_entity_id = er.async_validate_entity_id(
+        registry, config_entry.options[CONF_SOURCE_SENSOR]
     )
 
+    device_info = None
+    cron_pattern = None
+    delta_values = config_entry.options[CONF_METER_DELTA_VALUES]
+    meter_offset = timedelta(days=config_entry.options[CONF_METER_OFFSET])
+    meter_type = config_entry.options[CONF_METER_TYPE]
+    nudge_goal: int = config_entry.options[CONF_NUDGE_GOAL]
+    nudge_goal_type = config_entry.options[CONF_GOAL_TYPE]
+    meter_type = DAILY
+    name = config_entry.title
+    net_consumption = config_entry.options[CONF_METER_NET_CONSUMPTION]
+    periodically_resetting = config_entry.options[CONF_METER_PERIODICALLY_RESETTING]
+    sensor_always_available = config_entry.options.get(
+        CONF_SENSOR_ALWAYS_AVAILABLE, False
+    )
 
-class IntegrationBlueprintSensor(IntegrationBlueprintEntity, SensorEntity):
-    """powernudge Sensor class."""
+    device_info = DeviceInfo(
+        identifiers={("EntryId",entry_id)},
+    )
+
+    nudges = []
+    label_registry = lr.async_get(hass)
+    name_of_label = "test" #config_entry.data.get(CONF_SOURCE_PERSON).__str__()
+    #energy_goal = config_entry.data.get(CONF_NUDGE_GOAL)
+    #energy_goal_type = config_entry.data.get(CONF_GOAL_TYPE)
+    #unique_id = f"{config_entry.entry_id}_{energy_goal_type}"
+    label_for_entities = None
+    if not (
+        label_for_entities := label_registry.async_get_label_by_name(name_of_label)
+    ):
+        label_for_entities = label_registry.async_create(
+            name=name_of_label, color="red"
+        )
+    label_ids = {label_for_entities.label_id}
+
+    params_utility_meter = {
+        "cron_pattern": cron_pattern,
+        "delta_values": delta_values,
+        "meter_offset": meter_offset,
+        "meter_type": meter_type,
+        "name": name,
+        "net_consumption": net_consumption,
+        "parent_meter": entry_id,
+        "periodically_resetting": periodically_resetting,
+        "source_entity": source_entity_id,
+        "tariff_entity": None,  # Wert bleibt None
+        "tariff": None,  # Wert bleibt None
+        "unique_id": entry_id,
+        "device_info": device_info,
+        "sensor_always_available": sensor_always_available,
+    }
+    nudges.append(
+        Nudge(
+            label_ids=label_ids,
+            nudge_goal=nudge_goal,
+            nudge_goal_type=nudge_goal_type,
+            kwargs=params_utility_meter,
+        )
+    )
+
+    async_add_entities(nudges)
+
+
+class Nudge(UtilityMeterSensor):
+    _attr_should_poll = False
+
+    _attr_device_class = SensorDeviceClass.ENERGY
 
     def __init__(
-        self,
-        coordinator: BlueprintDataUpdateCoordinator,
-        entity_description: SensorEntityDescription,
+        self, label_ids: set[str], nudge_goal: int, nudge_goal_type: str, **kwargs
     ) -> None:
-        """Initialize the sensor class."""
-        super().__init__(coordinator)
-        self.entity_description = entity_description
+        super().__init__(**kwargs)
+        self._attr_name = "Power"
+        self._attr_extra_state_attributes = {
+            CONF_NUDGE_GOAL: nudge_goal,
+            CONF_GOAL_TYPE: nudge_goal_type,
+        }
+        self.person_labels = label_ids
 
     @property
-    def native_value(self) -> str | None:
-        """Return the native value of the sensor."""
-        return self.coordinator.data.get("body")
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    async def async_added_to_hass(self) -> None:
+        """
+        Run when entity about to be added to hass.
+
+        To be extended by integrations.
+        """
+        registry = er.async_get(self.hass)
+        registry.async_update_entity(self.entity_id, labels=self.person_labels)
+
+    async def async_will_remove_from_hass(self) -> None:
+        """
+        Run when entity will be removed from hass.
+
+        To be extended by integrations.
+        """
+
+    @callback
+    def async_registry_entry_updated(self) -> None:
+        """
+        Run when the entity registry entry has been updated.
+
+        To be extended by integrations.
+        """
+
+    async def async_removed_from_registry(self) -> None:
+        """
+        Run when entity has been removed from entity registry.
+
+        To be extended by integrations.
+        """
