5fe025b9e65da3a1ef968d4e1e364ac524c70ca7
FrederikJobst <frederik@klesberg.de>
2024-07-12 12:15:20 +0000

    removed Options Flow from nudgeplatform

--- custom_components/nudgeplatform/config_flow.py
+++ custom_components/nudgeplatform/config_flow.py
@@ -17,37 +17,6 @@ USER_SCHEMA = vol.Schema(
 
     )
 
-BUDGET_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_NUDGE_PERSON): selector.EntitySelector(
-            selector.EntitySelectorConfig(
-                domain=SENSOR_DOMAIN,
-                multiple=False,
-                filter=selector.EntityFilterSelectorConfig(
-                    integration=DOMAIN
-                ),
-            )
-        ),
-        vol.Required(CONF_BUDGET_YEARLY): selector.NumberSelector(
-            selector.NumberSelectorConfig(
-                min=1000,
-                max=10000,
-                mode=selector.NumberSelectorMode.SLIDER,
-                unit_of_measurement="kWh",
-            )
-        ),
-        vol.Required(CONF_TRACKED_SENSOR_ENTITIES): selector.EntitySelector(
-            selector.EntitySelectorConfig(
-                domain=SENSOR_DOMAIN,
-                multiple=True,
-                filter=selector.EntityFilterSelectorConfig(
-                    device_class=SensorDeviceClass.ENERGY
-                ),
-            )
-        ),
-    }
-)
-
 
 class Options(Enum):
     USER = "user"
@@ -76,28 +45,5 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=USER_SCHEMA,
             errors=errors,
         )
-    @staticmethod
-    @callback
-    def async_get_options_flow(
-        config_entry: config_entries.ConfigEntry,
-    ) -> config_entries.OptionsFlow:
-        """Create the options flow."""
-        return OptionsFlowHandler(config_entry)
 
 
-class OptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Manage the options."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-            
-        return self.async_show_form(
-            step_id="init",
-            data_schema=BUDGET_SCHEMA
-            )
